d496636cb6b71a68b049adc17fba4c4ced7d16ea
griffinacurtiss <griffinacurtiss@gmail.com>
2021-01-22 15:58:08 -0800

    Add files via upload

---
+++ student.cpp
@@ -0,0 +1,14 @@
+#include "student.h"
+
+void student::setStudentId(string id){
+    studentId = id;
+}
+
+void student::setFirstName(string firstName){
+    firstName = firstName;
+}
+
+void student::setLastName(string lastName){
+    lastName = lastName;
+}
+
