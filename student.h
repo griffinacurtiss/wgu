03732ef047f3e0c963be36804ce236797f41283c
griffinacurtiss <griffinacurtiss@gmail.com>
2021-01-22 14:36:00 -0800

    Add files via upload

---
+++ student.h
@@ -0,0 +1,13 @@
+#include <string>
+using namespace std;
+class Student{
+  private: 
+  string studentId;
+  string firstname;
+  string lastname;
+  string  emailAddress;
+  int age;
+  int numberOfDaystoComplete[];
+  string degreeProgram;
+};
+
